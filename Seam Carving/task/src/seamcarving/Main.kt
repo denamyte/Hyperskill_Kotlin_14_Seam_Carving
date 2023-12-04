bec28e1fc48a6a7b0d3378e572aabfadb717d5b8
denys <denamyte@gmail.com>
2023-12-04 18:46:02 +0200

    2023-12-04: Stage 1/6

--- Seam Carving/task/src/seamcarving/Main.kt
+++ Seam Carving/task/src/seamcarving/Main.kt
@@ -1,5 +1,15 @@
 package seamcarving
 
 fun main() {
-    print("Hello, World!")
+    val width = inputInt("Enter rectangle width:")
+    val height = inputInt("Enter rectangle height:")
+    println("Enter output image name:")
+    val fileName = readln()
+
+    Stage1(width, height, fileName).run()
+}
+
+private fun inputInt(msg: String): Int {
+    println(msg)
+    return readln().toInt()
 }
